d332bacdbc6bcbcbc7e2df1f0e16837ac599b047
Ben McConville <benjamin.j.mcconville@gmail.com>
2024-01-24 14:04:44 +0000

    Chap 4

--- Chapter04/OneDKalmanFilter.py
+++ Chapter04/OneDKalmanFilter.py
@@ -2,42 +2,8 @@ import matplotlib.pyplot as plt
 from numpy.random import randn
 import math
 
-class dog:
-    positions = []
-    currentPosition = 0
-    previousMove = 0
-    jD = 1
-    def __init__(self, n=[1, 1, 0, 0, 0, 0, 0, 0, 1, 0]):
-        self.positions = n
-    def move(self):
-        if (randn() > 0):
-            self.previousMove = self.jD
-            self.currentPosition = (self.currentPosition + self.jD) % len(self.positions)
-        else:
-            self.previousMove = -self.jD
-            self.currentPosition = (self.currentPosition - self.jD ) % len(self.positions)
 
-    def getSensorReading(self):
-        return self.positions[self.currentPosition], self.previousMove
-    
-    def getErrorSensorReading(self):
-        randomVal = randn()
-        if randomVal < -1.5:
-            return self.positions[self.currentPosition -1], self.previousMove
-        if randomVal > 1.5:
-            return self.positions[self.currentPosition +1], self.previousMove
-        return self.getSensorReading()
-
-
-
-def plotDis(mean, stan):
-    x = [mean + i/10. for i in range(-100, 100)]
-    y = []
-    for i in x:
-        val = 2.713**((-(i - mean)**2)/(2*(stan**2))) /(stan* math.sqrt(2*3.14159))
-        y.append(val)
-    plt.plot(x, y)
-    plt.show()
+speed = 10.
 
 def predict(m1, v1, m2, v2):
     return (m1 + m2, v1 + v2)
@@ -47,18 +13,72 @@ def update(m1, v1, m2, v2):
     avgVar  = (v1 * v2) / (v1 + v2)
     return (avgMean, avgVar)
 
-Simon = dog()
+class dog:
+    process_var = 0.
+    sensor_var  = 0.
+    position    = 0.
 
-for i in range(5):
-    Simon.move()
-    Simon.getSensorReading()
+    def __init__(self, pv, sv, pos):
+        self.process_var = pv
+        self.sensor_var  = sv
+        self.position    = pos
+
+    def move(self, epoch):
+        self.position = self.position + (epoch * speed)
+        self.position += randn() * self.process_var# Add Process Noise
+
+    def getSensor(self):
+        return self.position + randn() * self.sensor_var / 2.
+
+class KF:
+    mean = 0.
+    var  = 0.
+    process_var = 0.
+    sensor_var  = 0.
+
+    def __init__(self, pv, sv, startVar):
+        self.process_var = pv
+        self.sensor_var  = sv
+        self.var         = startVar
+
+    def posPredict(self, epoch):
+        self.mean, self.var = predict(self.mean, self.var, speed * epoch, self.process_var)
+    
+    def posUpdate(self, z):
+        self.mean, self.var = update(self.mean, self.var, z, self.sensor_var)
+
+
+def plotDis(mean, stan):
+    x = [mean + i/10. for i in range(-100, 100)]
+    y = []
+    for i in x:
+        val = 2.713**((-(i - mean)**2)/(2*(stan**2))) /(stan* math.sqrt(2*3.14159))
+        y.append(val)
+    plt.xlim(-10, 200)
+    plt.ylim(0, 3)
+    plt.plot(x, y)
+    plt.show()
+
+def plotPoints(y):
+    plt.plot(y)
+    plt.show()
 
 
 
-plotDis(10,1)
+Simon = dog(4., 4., 0.)
+Filter = KF(4., 4., 10.)
 
-print(update(10., .2**2, 11., .1**2))
+points = []
+for i in range(10):
+    print(Filter.mean, Filter.var)
+    plotDis(Filter.mean, Filter.var)
+    Simon.move(1.)
+    Filter.posPredict(1.)
+    Filter.posUpdate(Simon.getSensor())
 
+#plotPoints(points)
+
+#plotDis(10,1)
 
 
 
